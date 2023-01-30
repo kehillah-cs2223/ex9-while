d26fb119bc4154380a5c16884055398960d62635
Erin Bennett <ebennett@kehillah.org>
2023-01-30 13:36:19 -0800

    point to places to edit

--- q1.py
+++ q1.py
@@ -1,8 +1,7 @@
 """
 Question 1
 
-Rewrite the `draw_rectangular_spiral` function so that it draws a spiral that
-goes on forever.
+tl;dr Rewrite with `while` and make it go on **forever**
 
 Hint: Remember to remove the parameter
 
@@ -10,20 +9,28 @@ This is function from Textbook Exercises #2 that draws a rectangular spiral.
 Before, we only knew for loops, so we had to say what the max_side length was.
 
 But now we know about `while` loops!
+
+Make a new version of draw_rectangular_spiral that keeps spiralling outwards
+until the program is closed.
 """
 
 from turtle import *
 
+
+####### EDIT THIS FUNCTION DECLARATION #######
+# Remove the parameter `max_side`
 def draw_rectangular_spiral(max_side):
     """Draw a rectangular spiral, starting in the center.
     
     `max_side` is the length of the longest side of the spiral in pixels"""
-
-    # Remember: When range takes 2 parameters, the first parameter is the start
-    # and the second parameter is the end. So this range will start at 1 and
-    # stop before it gets to max_side + 1 (i.e. it will stop at max_side)
+    
+    ####### EDIT THIS FOR LOOP! #######
+    # Rewrite with `while` and make it go on **forever**
     for side_length in range(1, max_side + 1, 5):
         forward(side_length)
         right(90)
 
+####### EDIT THIS FUNCTION CALL #######
+# Remove the argument
 draw_rectangular_spiral(100)
+
